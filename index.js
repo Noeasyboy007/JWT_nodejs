7fe4e1d4a1bfcbbf1ee939e8d44d83c06e98485b
Aritra Bera <aritrabera69@gmail.com>
2024-08-01 03:51:14 +0530

    mongoDB connection

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const dotenv = require("dotenv");
 
 const colors = require("colors");
 
+const db = require("./config/db");
+
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,async()=>{console.log(`Server Started ap PORT ${PORT}`.bgBlue);})
+app.listen(PORT,async()=>{console.log(`Server Started ap PORT ${PORT}`.bgBlue);
+    await db();
+})
 
 
